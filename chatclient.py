88cdb35fa64e23b5c3b0120f6c82242c34c6b270
Anish Sheth <anishsheth05@gmail.com>
2022-01-01 17:08:29 -0600

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -1,1 +1,18 @@
+import PySimpleGUI as sg
 
+sg.theme('DarkAmber')
+
+layout = [
+    [sg.Text('Text on row 1')],
+    [sg.Text('Enter chat here:'), sg.InputText()],
+    [sg.Button('Send'), sg.Button('Exit')]
+]
+
+window = sg.Window('JankChat v0.0.1', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+window.close()
