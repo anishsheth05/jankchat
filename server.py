95e99260a8454ce11d1666d73ca5eaf666ac3e30
Anish Sheth <anishsheth05@gmail.com>
2022-01-01 17:16:28 -0600

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def send(msg): #this sends the msg to all the clients
     clients.send(msg.encode())
     
 def listen(client,address): 
-   print ("Accepted connection from: " + address)
+    print ("Accepted connection from: " + address)
     clients.append(client)
     while True:
         data = client.recv(1024)
@@ -31,11 +31,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # now listen to the web server on port 80 - the normal http port
     s.listen(5)
     while True:
-       print "Server is listening for connections..."
+        print ("Server is listening for connections...")
         client, address = s.accept()
-       th.append(Thread(target=listen, args = (client,address)).start()) #threaded stuff it makes a thread for each client and makes it do listen
+        th.append(Thread(target=listen, args = (client,address)).start()) #threaded stuff it makes a thread for each client and makes it do listen
 
     s.close()
-   
-        
-      
