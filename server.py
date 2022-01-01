8aeb4fb6d86443a455c4696463c1116bb405a5a5
SXK <61637558+Snipexkillo@users.noreply.github.com>
2022-01-01 17:23:43 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,31 @@
-
 import socket
 from threading import Thread
-import thread
+import threading
 
 HOST = '127.0.0.1'
 PORT = 80
-messages = [] #list of all the msgs, unnecessary but there anyways
-clients = [] #list of all the clients
-th = [] #all the threads
-def send(msg): #this sends the msg to all the clients
-  for client in clients:
-    clients.send(msg.encode())
-    
-def listen(client,address): 
-    print ("Accepted connection from: " + address)
-    clients.append(client)
+messages = []  # list of all the msgs, unnecessary but there anyways
+clients = []  # list of all the clients
+th = []  # all the threads
+
+
+def send(msg):  # this sends the msg to all the clients
+    for c in clients:
+        c.send(msg.encode())
+
+
+def listen(cli, addr):
+    print("Accepted connection from: " + addr)
+    clients.append(cli)
     while True:
-        data = client.recv(1024)
+        data = cli.recv(1024)
         if not data:
             break
         else:
             messages.append(data)
             send(data)
-    client.close()
-            
+    cli.close()
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))  # the bind puts the socket on port 80 on
@@ -31,8 +33,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # now listen to the web server on port 80 - the normal http port
     s.listen(5)
     while True:
-        print ("Server is listening for connections...")
+        print("Server is listening for connections...")
         client, address = s.accept()
-        th.append(Thread(target=listen, args = (client,address)).start()) #threaded stuff it makes a thread for each client and makes it do listen
+        th.append(Thread(target=listen, args=(
+        client, address)).start())  # threaded stuff it makes a thread for each client and makes it do listen
 
     s.close()
