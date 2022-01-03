fb1ca1ce3576d9377ef7232c023039b4a8351144
SXK <61637558+Snipexkillo@users.noreply.github.com>
2022-01-03 15:36:38 -0600

    Update server.py

--- server.py
+++ server.py
@@ -38,7 +38,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         client, address = s.accept()
         th.append(Thread(target=listen, args=(
         client, address, len(clients)+1)).start())  # threaded stuff it makes a thread for each client and makes it do listen
-        client.send("Welcome Client {}".format(len(clients)+1).encode())
+        for c in clients:
+            c.send("Client {} has joined the chat".format(len(clients)+1).encode())
 
 
     s.close()
