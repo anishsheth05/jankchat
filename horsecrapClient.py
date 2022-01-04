d6fe72e5c77de572e7ab7be37573d322cc5ea26f
Anish Sheth <anishsheth05@gmail.com>
2022-01-04 15:58:40 -0600

    finished pig latin
    -also, input bar now is properly sized and clears input after sending

--- horsecrapClient.py
+++ horsecrapClient.py
@@ -18,7 +18,7 @@ msgd = False
 layout = [
     [sg.Multiline(chatbox, size=(200, 40), key='Text', autoscroll=True,
                   focus=False, disabled=True)],
-    [sg.Text('Enter chat here:'), sg.Input(do_not_clear=False, )],
+    [sg.Text('Enter chat here:'), sg.Input(do_not_clear=True, key='Input', size = (185, 1))],
     [sg.Button('Send'), sg.Button('Exit')]
 ]
 
@@ -26,7 +26,7 @@ layout = [
 window = sg.Window('JankChat v0.0.1', layout)
 window.read(timeout=50)
 textbox = window['Text']
-
+inputbox = window['Input']
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
@@ -51,6 +51,7 @@ def receiving():
             # else:
             chatbox += '\n' + incoming + '\n'   # adding a newline to it
             textbox.update(chatbox)
+            inputbox.update('')
 
 
 close = False
@@ -63,19 +64,37 @@ while True:
         s.close()
         break  # exit loop if 'x' or exit button clicked
     if event == 'Send':
-        msg: str = values[0]
+        msg: str = values['Input']
         if msg[:1] == '/':
-            if msg[1:8] == 'piglatin':
-                words = msg[9:].split(sep=' ')
-                for i in range(words.len()):
+            print('command found')
+            if msg[1:9] == 'piglatin':
+                print('piglatin is the command')
+                words = msg[10:].split(sep=' ')
+                for i in range(0, len(words)):
                     word = words[i]
-                    if word[0] == 'a' or word[0] == 'e' or word[0] == 'i' \
-                       or word[0] == 'o' or word[i] == 'u':
-                       words[i] = words[i] + 'yay'
+                    if word[0:1] == 'a' or word[0:1] == 'e' or word[0:1] == 'i' \
+                       or word[0:1] == 'o' or word[0:1] == 'u' or word[0:1] == 'A' \
+                       or word[0:1] == 'E' or word[0:1] == 'I' or word[0:1] == 'O' \
+                       or word[0:1] == 'U':
+                        words[i] = words[i] + 'yay '
+                    elif word[0:1] == 'y' or word[0:1] == 'Y':
+                        words[i] = words[i][1:] + words[i][0:1] + 'ay'
                     else:
-                        
-                       
-
+                        vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']
+                        first_vowel: int = None
+                        for vowel in vowels:
+                            index = word.find(vowel)
+                            if index == -1:
+                                continue
+                            if first_vowel is None:
+                                first_vowel = word.find(vowel)
+                            else:
+                                first_vowel = min(word.find(vowel), first_vowel)
+                        words[i] = words[i][first_vowel:] + words[i][0:first_vowel] + 'ay '
+                msg = ''.join(words)
+                s.send(msg.encode())
+            else:
+                s.send(msg.encode())
         else:
             s.send(msg.encode())  # sends encoded messages
             # values[0] = ''      # trying to clear input after sending msg
